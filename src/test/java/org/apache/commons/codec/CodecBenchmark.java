7684e97090460c8d15157911c2e4e1025da68d61
MalikUsama27 <Usamamalik7008@gmail.com>
2025-01-05 02:11:02 +0500

    JHM

--- src/test/java/org/apache/commons/codec/CodecBenchmark.java
+++ src/test/java/org/apache/commons/codec/CodecBenchmark.java
@@ -14,8 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.commons.codec;
 
+package org.apache.commons.codec;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
 
@@ -31,7 +31,6 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
-
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 @State(Scope.Thread)
@@ -39,11 +38,9 @@ import org.openjdk.jmh.annotations.Warmup;
 @Warmup(iterations = 3)
 @Measurement(iterations = 5)
 public class CodecBenchmark {
-
     private Base64 base64;
     private Hex hex;
     private byte[] data;
-
     @State(Scope.Thread)
     public static class BenchmarkState {
         byte[] smallData = "Hello World!".getBytes(StandardCharsets.UTF_8);
@@ -54,33 +51,27 @@ public class CodecBenchmark {
             }
         }
     }
-
     @Setup
     public void setup() {
         base64 = new Base64();
         hex = new Hex();
     }
-
     @Benchmark
     public byte[] encodeBase64(BenchmarkState state) {
         return base64.encode(state.smallData);
     }
-
     @Benchmark
     public byte[] decodeBase64(BenchmarkState state) {
         return base64.decode(base64.encode(state.smallData));
     }
-
     @Benchmark
     public byte[] encodeHex(BenchmarkState state) {
         return hex.encode(state.smallData);
     }
-
     @Benchmark
     public byte[] decodeHex(BenchmarkState state) throws DecoderException {
         return hex.decode(hex.encode(state.smallData));
     }
-
     public static void main(String[] args) throws Exception {
         org.openjdk.jmh.runner.options.Options opt = new org.openjdk.jmh.runner.options.OptionsBuilder()
                 .include(CodecBenchmark.class.getSimpleName())
